facb409efe869029fa9d12f1081a7679402ba264
Nguyễn Dũng <dpron12345@gmail.com>
2022-01-11 00:23:39 +0700

    remove comment

--- lib/models/shop_details.dart
+++ lib/models/shop_details.dart
@@ -1,32 +1,3 @@
-// class ShopDetailsModel {
-//   final String shopName;
-//   final String shopCoverImage;
-//   final List shopMenu;
-//   final Map businessHours;
-//   final String? shopLocation;
-//   final String latitude, longitude;
-//   final num? shopRating;
-//   final int? ratingCount;
-//   final List? shopVouchersID;
-//   final bool? isPromo;
-//   final bool? isParner;
-
-//   ShopDetailsModel({
-//     required this.shopName,
-//     required this.shopCoverImage,
-//     required this.shopMenu,
-//     required this.businessHours,
-//     required this.latitude,
-//     required this.longitude,
-//     this.shopLocation,
-//     this.shopRating,
-//     this.ratingCount,
-//     this.shopVouchersID,
-//     this.isPromo = false,
-//     this.isParner = false,
-//   });
-// }
-
 import 'package:baemin/models/shop.dart';
 
 class ShopDetailsModel extends ShopModel {
