72c9a3d8aead92b9d4acb9b65a6f39de21ee1ae5
Nguyễn Dũng <dpron12345@gmail.com>
2022-01-11 00:24:03 +0700

    Update order_details.dart

--- lib/models/order_details.dart
+++ lib/models/order_details.dart
@@ -1,43 +1,45 @@
+import 'package:baemin/models/order.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class OrderDetailsModel {
+class OrderDetailsModel extends OrderModel {
   final int discountPrice;
   final List dishes;
-  final int dishQuantity;
   final bool haveVoucher;
-  final bool isDelivered;
-  final String orderID;
   final String ordererAddress;
   final String ordererName;
   final int ordererPhoneNumber;
-  final String paymentType;
   final int shippingFee;
   final int minimumOrderFee;
   final int serviceFee;
   final num shippingDistance;
   final String shopID;
-  final String shopName;
-  final Timestamp timestamp;
-  final int totalPrice;
 
   OrderDetailsModel({
     required this.discountPrice,
     required this.dishes,
-    required this.dishQuantity,
     required this.haveVoucher,
-    required this.isDelivered,
-    required this.orderID,
     required this.ordererAddress,
     required this.ordererName,
     required this.ordererPhoneNumber,
-    required this.paymentType,
     required this.shippingFee,
     required this.minimumOrderFee,
     required this.serviceFee,
     required this.shippingDistance,
     required this.shopID,
-    required this.shopName,
-    required this.timestamp,
-    required this.totalPrice,
-  });
+    required int dishQuantity,
+    required bool isDelivered,
+    required String orderID,
+    required String paymentType,
+    required String shopName,
+    required Timestamp timestamp,
+    required int totalPrice,
+  }) : super(
+          dishQuantity: dishQuantity,
+          isDelivered: isDelivered,
+          orderID: orderID,
+          paymentType: paymentType,
+          shopName: shopName,
+          timestamp: timestamp,
+          totalPrice: totalPrice,
+        );
 }
